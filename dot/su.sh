0e3d44e352d6e844be7a82d6b6a735bf783b8b50
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-06-11 08:56:21 -0700

    update findscan

---
+++ dot/su.sh
@@ -0,0 +1,26 @@
+# ID and sudo
+
+
+  # <> Obligatory debugging block
+  #_full_xtrace
+  : "${nBS[0]}:${nL} ${nBS[1]}:${nBL[0]}"
+  #exit "${nL}"
+  #set -x
+
+
+if [[ "${UID}" == 0 ]]; then
+  printf '\n\t Must be a regular user and use sudo. \n\n'
+  exit 1
+elif ! sudo -v; then
+  printf '\n\tValidation failed of user\x27s \x60sudo\x60 timestamp; '
+  printf 'exiting.\n\n'
+  exit "${nL}"
+fi
+
+
+  # <> Obligatory debugging block
+  #_full_xtrace
+  : "${nBS[0]}:${nL} ${nBS[1]}:${nBL[0]}"
+  #exit "${nL}"
+  #set -x
+
