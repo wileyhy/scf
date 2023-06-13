402ff9451cee8e56a6284a8713ceed9a4bae53ac
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-06-12 20:40:20 -0700

    update scf

---
+++ dot/verify-dacs.sh
@@ -0,0 +1,22 @@
+# Verify DAC's
+
+
+
+: 'Verify DACs'
+# Bug: var path_2 is now loc_path_2
+namei_o="$(
+  for d in "${bash_path[@]}"; do
+    namei -xl "$(
+      realpath -e "$d" 2>/dev/null
+    )"
+  done \
+    | grep -v ^'f:' \
+    | gawk --lint '$2 !~ /root/ || $3 !~ /root/ { print }'
+)"
+if [[ -n "${namei_o}" ]]; then
+  echo 'A directory in PATH is not fully owned by root (DAC).'
+  echo "${namei_o}"
+  exit "${nL}"
+fi
+
+
