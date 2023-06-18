5d95318f786a16ffd1b1141004f7e973f1c5e4dc
Wiley Young <84648683+wileyhy@users.noreply.github.com>
2023-06-18 08:21:44 -0700

    update scf

--- gardening.sh
+++ gardening.sh
@@ -4,7 +4,7 @@
 
   # <> Obligatory debugging block
   : "${nBS[0]}:${nL} ${nBS[1]}:${nBL[0]}"
-  _post_src "${nL}" "$@"
+  _post_src "${nBS[0]}" "${nL}" "$@"
   #_full_xtrace
   : "${nBS[0]}:${nL} ${nBS[1]}:${nBL[0]}"
   #exit "${nL}"
